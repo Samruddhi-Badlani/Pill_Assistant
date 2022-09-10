3b3e7897a2d58e0555be920d68ef1ed7fbbd6c7d
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2022-09-11 01:17:06 +0530

    completed login and register pages frontend

---
+++ lib/pages/bgImage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BgImage extends StatelessWidget {
+  const BgImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      "assets/bg.png",
+      fit: BoxFit.cover,
+      color: Colors.black.withOpacity(0.7),
+      colorBlendMode: BlendMode.darken,
+    );
+  }
+}

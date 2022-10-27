aa19e317d22efdb0c8c425f10035ab3a88a2a15d
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2022-10-27 17:26:00 +0530

    added qr code scanning through camera

--- lib/pages/scanQR.dart
+++ lib/pages/scanQR.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'dart:io';
 
@@ -49,7 +51,7 @@ class _ScannerState extends State<Scanner> {
       var uriloaded = Uri.parse('http://api.qrserver.com/v1/read-qr-code/');
 
       PickedFile? pickedImage =
-          await ImagePicker().getImage(source: ImageSource.gallery);
+          await ImagePicker().getImage(source: ImageSource.camera);
 
       var request2 = http.MultipartRequest('POST', uriloaded);
 
@@ -67,6 +69,12 @@ class _ScannerState extends State<Scanner> {
               File(croppedImage.path).lengthSync(),
               filename: croppedImage.path.split("/").last);
 
+          final result = await ImageGallerySaver.saveImage(
+              await (File(croppedImage.path).readAsBytes()),
+              quality: 60,
+              name: 'letsseecroppedimage');
+          print("saved cropped image to gallery " + result.toString());
+
           //request2.fields["MAX_FILE_SIZE"] = "4048576";
 
           request2.files.add(await multipartFile);
@@ -78,6 +86,8 @@ class _ScannerState extends State<Scanner> {
           request2.headers["Content-Length"] =
               request2.contentLength.toString();
           request2.headers["Content-Type"] = "multipart/form-data";
+          request2.headers["Accept"] = "*/*";
+          request2.headers["Accept-Encoding"] = "gzip, deflate, br";
 
           print('directory is ' + pickedImage.path);
           print("size of file is " +
@@ -99,34 +109,38 @@ class _ScannerState extends State<Scanner> {
           for (var myMed in myData) {
             print(myMed["symbol"][0]["data"]);
 
-            setState(() {
-              medicineId = myMed["symbol"][0]["data"];
-            });
+            if (myMed["symbol"][0]["data"] != null) {
+              setState(() {
+                medicineId = myMed["symbol"][0]["data"];
+              });
+            }
           }
           print('json decoded data' + myData.toString());
 
           final queryParameters = {'type': 'user'};
 
-          var uri = Uri.https(
-              'pill-management-backend.herokuapp.com',
-              "/mobile-app-ws/users/${userId}/medicine/${medicineId}",
-              queryParameters);
+          if (medicineId != null) {
+            var uri = Uri.https(
+                'pill-management-backend.herokuapp.com',
+                "/mobile-app-ws/users/${userId}/medicine/${medicineId}",
+                queryParameters);
 
-          final http.Response getResponse = await http.get(
-            uri,
-            headers: <String, String>{
-              'Content-Type': 'application/json; charset=UTF-8',
-              'authorization': prefs.get('authToken').toString(),
-              'userid': userId
-            },
-          );
+            final http.Response getResponse = await http.get(
+              uri,
+              headers: <String, String>{
+                'Content-Type': 'application/json; charset=UTF-8',
+                'authorization': prefs.get('authToken').toString(),
+                'userid': userId
+              },
+            );
 
-          print(getResponse.body);
-          setState(() {
-            myMedicineScannedData = json.decode(getResponse.body);
-          });
-          print(getResponse.statusCode);
-          print(getResponse.headers);
+            print(getResponse.body);
+            setState(() {
+              myMedicineScannedData = json.decode(getResponse.body);
+            });
+            print(getResponse.statusCode);
+            print(getResponse.headers);
+          } else {}
         }
       }
     }
